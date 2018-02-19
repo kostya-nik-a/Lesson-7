0dcb747fbb94313a136bce0a2a0ecd5945d615eb
kostya-nik-a <35659234+kostya-nik-a@users.noreply.github.com>
2018-02-19 23:22:19 +0700

    Update test.php

--- test.php
+++ test.php
@@ -15,55 +15,22 @@ $tests = json_decode($contents, true);
 $trueAnswer = 0;
 $falseAnswer = 0;
 $noAnswer = 0;
-$userAnswers = [];
-$i = 0;
 
-foreach ($tests as $userAnswers) {
-    if (empty($_POST)) {   
-    } 
-    elseif (empty($_POST["answerUser$i"])) {
-            $noAnswer++;            
-    } elseif ($_POST["answerUser$i"] == $userAnswers["correct-answer"]) {
-            $trueAnswer++;            
-    } else {
-            $falseAnswer++;            
-    }
+foreach ($tests as $qkey => $questions) {
+    if (is_array($questions)) {
+        foreach ($questions as $answers) {
+            if (empty($_POST)) {   
+            } 
+            elseif (empty($_POST["answerUser$i"])) {
+                $noAnswer++;            
+            } elseif ($_POST["answerUser$i"] == $answers["correct-answer"]) {
+                $trueAnswer++;            
+            } else {
+                $falseAnswer++;            
+            }
             $i++; 
-}
-
-if (!empty($_POST)) {
-    $userName = $_POST['user_name']; 
-
-if (empty($_POST['user_name'])) {
-    http_response_code(400);
-    echo "Введите фамилилю и имя.";
-}
-
-$image = imagecreatetruecolor(965, 685);
-$backcolor = imagecolorallocate($image, 255, 255, 555);
-$textcolor = imagecolorallocate($image, 50, 50, 50);
-
-$boxFile = __DIR__ . '/sertificate.jpg';
-if (!file_exists($boxFile)) {
-    echo "Файл с картинкой не найден";
-    exit();
-}
-
-$imBox = imagecreatefromjpeg($boxFile);
-imagefill($image, 0, 0, $backcolor);
-imagecopy($image, $imBox, 0, 0, 0, 0, 965, 685);
-
-$fontFile = __DIR__ . '/timesbi.ttf';
-if (!file_exists($fontFile)) {
-    echo "Файл со шрифтом не найден";
-    exit();
-}
-
-imagettftext($image, 26, 0, 400, 300, $textcolor, $fontFile, $userName);  
-imagettftext($image, 16, 0, 450, 350, $textcolor, $fontFile, $trueAnswer);
-header('Content-type: image/jpeg');
-imagejpeg($image); 
-
+        }
+    }
 }
 
 
@@ -77,31 +44,55 @@ imagejpeg($image);
     </head>
     <body>
         <h1>Вы проходите тест: <?php echo '<i style="color: blue;">'.$tests['title'].'</i>' ?> </h1>
-        <form action="" method="POST">
-            <div style="text-align: left;">
+        <form action="image.php" method="POST">
+            <div style="width: 50%; text-align: left;">
                 <?php 
-                    $i = 0;
-                    foreach ($tests as $userAnswers) {
-                ?>
-                <fieldset>
+                        foreach ($tests as $qkey => $questions) {
+                            if (is_array($questions)) {
+                                foreach ($questions as $answers) {
+                    ?>
+                    <fieldset>
                     <legend>
-                        <h2><?php echo $userAnswers['question'] ?></h2>
+                        <h3><?php echo $answers['question'] ?></h3>
                     </legend>
-                <label>
-                    <input name="<?php echo 'answerUser'.$i ?>" type="radio" value="<?php echo $userAnswers['answer1']; ?>"><?php echo $userAnswers['answer1']; ?>
-                    <input name="<?php echo 'answerUser'.$i ?>" type="radio" value="<?php echo $userAnswers['answer2']; ?>"><?php echo $userAnswers['answer2']; ?>
-                    <input name="<?php echo 'answerUser'.$i ?>" type="radio" value="<?php echo $userAnswers['answer3']; ?>"><?php echo $userAnswers['answer3']; ?>
-                    <input name="<?php echo 'answerUser'.$i ?>" type="radio" value="<?php echo $userAnswers['answer4']; ?>"><?php echo $userAnswers['answer4']; ?>
-                </label>
+                    <label>
+                        <?php 
+                            for ($i=0; $i < count($answers['answers']);$i++) { 
+                        ?>
+                    <input name="<?php echo 'answerUser'.$i ?>" type="radio" value="<?php echo $answers['answers'][$i]; ?>"><?php echo $answers['answers'][$i]; ?>
+                    <?php 
+                        }
+                    ?>
+                    </label>
                 </fieldset>
                 <?php
-                $i++;
+                        }
+                    }
                 }
+
+                ?>
+                <?php 
+                if (!empty($_POST)) {
+                echo  
+                "<p> Количество правильных ответов: " . $trueAnswer . "</p>" .
+                "<p> Количество НЕ правильных ответов: " . $falseAnswer . "</p>" .
+                "<p> Нет ответов на " .$noAnswer. " вопросов </p>";
+                }  
                 ?>
 
-                <label> Введите свое имя и фамилию <input  name="user_name" type="text" value=""></label><br> 
-                <button type="submit">Результат</button>   
-                                       
+                <p><strong>Введите ваши фамилию и имя: </strong></p>
+                <input name="user_name" type="text" value="">
+                <button type="submit">Результат</button>  
+
+                <?php 
+                   /* if (empty($_POST['user_name']) && !empty($_POST)) {
+                        http_response_code(400);
+                        echo "<p style='color: red;'>Введите фамилилю и имя.</p>";
+                     } 
+
+                    $userName = $_POST['user_name'];
+                    echo $userName; */
+                ?>            
             </div>
         </form>
     </body>
