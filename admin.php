029482088610806152bdeac06f99c32fc4208fa8
kostya-nik-a <35659234+kostya-nik-a@users.noreply.github.com>
2018-02-26 21:04:55 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,96 +1,89 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-//echo "<pre>";
-//var_dump($_FILES);
-
-//$AdminUrl = $_SERVER['HTTP_HOST'] . $_SERVER["REQUEST_URI"];
-//$listURL = dirname($AdminUrl) . "/list.php";     
-
-//if(isset($_FILES["send_files"]) && !empty($_FILES["send_files"]) && $_FILES['send_files']['size'] !== 0) {
-//    header("Location: list.php", true, 307);        
-//} 
-
-if (!file_exists(__DIR__ . "/tests")) {
-    mkdir(__DIR__ . "/tests");
+$test_number = ($_GET['test_number']);
+$testDir = __DIR__ ."/tests";
+$tests_list = scandir($testDir);
+if ($test_number < 1 || !ctype_digit($test_number) || $test_number > count($tests_list) - 2) {
+   echo "Такого теста не существует. Выберите существующий номер теста на <a href='list.php'> предыдущей странице </a>";
+   exit();
 }
-$testDir = __DIR__."/tests";
-$message = '';
-if (isset($_FILES['send_files'])) {
-    $file = $_FILES['send_files'];
-}
-if (isset($file['name']) && !empty($file['name'])) 
-{
-  if ($file['error'] == UPLOAD_ERR_OK)
-      {
-      	$info = new SplFileInfo($file['name']);
-        $fileType = $info->getExtension();
-        
-        if ($fileType != "json") {
-    		echo "Ошибка загрузки файла. Необходимо загрузить только файлы с расширением json. <a href='admin.php'><br> Назад </a> ";
-    		exit();
-    	}
 
-    	move_uploaded_file($file['tmp_name'], $testDir.DIRECTORY_SEPARATOR.$file['name']);
-        header("Location: list.php", true, 307); 
-      	$message = 'Файл успешно загружен';
-      }
-      else {
-      	$message = 'Файл не загружен: ';
-      	echo $message.$file['error'];
-      }
+$test = $tests_list[$test_number+1];
+$contents = file_get_contents($testDir.DIRECTORY_SEPARATOR.$test);
+$tests = json_decode($contents, true);
+
+$trueAnswer = 0;
+$falseAnswer = 0;
+$noAnswer = 0;
+$i = 0;
+
+foreach ($tests as $qkey => $questions) {
+    if (is_array($questions)) {
+        foreach ($questions as $answers) {
+            if (empty($_POST)) {   
+            } 
+            elseif (empty($_POST["answerUser$i"])) {
+                $noAnswer++;            
+            } elseif ($_POST["answerUser$i"] == $answers["correct-answer"]) {
+                $trueAnswer++;            
+            } else {
+                $falseAnswer++;            
+            }
+            $i++; 
+        }
+    }
 }
+
 ?>
 
-<!DOCTYPE HTML>
-<html>
- <head>
-  <meta charset="utf-8">
-  <title>Lesson 6</title>
-</head>
-<style>
-  div
-  { display: inline-block;
-  	width: 50%;
-  	}
-legend { font-weight: 600;}
-</style>
-<body>
-<div>
-<form enctype="multipart/form-data" method="post" action="">
-	<fieldset>
-		<legend>Загрузка файлов</legend>
-			<p>Загрузите файлы с тестами в формаете JSON</p>
-			<input type="file" name="send_files" placeholder="Файл">
-			<hr>
-			<input type="submit" name="btn" value="Отправить файлы на сервер">
-			<p style="color: green;"><?php echo $message; ?>
-	</fieldset>
-	<fieldset>
-        <legend>Список загруженных файлов</legend>
-        <p>Json-файлы с тестами, загруженные на сервер:</p>
-        <ul>
-            	<?php 
-            		$filesDir = scandir($testDir);
-                    $numFiles = count(scandir($testDir))-2;
-                    $filesDirs = array_slice($filesDir, 2);
-            		
-                    foreach ($filesDirs as $fd)	{
-        				echo '<li>'.$fd.'</li>';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Test</title>
+    </head>
+    <body>
+        <h2>Вы проходите тест: <?php echo '<i style="color: blue;">'.$tests['title'].'</i>' ?> </h2>
+        <form action="" method="POST">
+            <div style="width: 50%; text-align: left;">
+                    <?php 
+                        foreach ($tests as $qkey => $questions) {
+                            if (is_array($questions)) {
+                                $x = 0;
+                                foreach ($questions as $answers) {
+                    ?>
+                    <fieldset>
+                    <legend>
+                        <h3><?php echo $answers['question'] ?></h3>
+                    </legend>
+                    <label>
+                        <?php 
+                        //$x = 0;
+                            for ($i=0; $i < count($answers['answers']); $i++) { 
+                        ?>
+                    <input name="<?php echo 'answerUser'.$x ?>" type="radio" value="<?php echo $answers['answers'][$i]; ?>"><?php echo $answers['answers'][$i]; ?>
+                    <?php 
+                       }  
+                    ?>
+                    </label>
+                </fieldset>
+                <?php
+                        $x++;}  
                     }
-        				
-        		?>
-        </ul>
+                }
 
+                ?>
+                <button type="submit">Результат</button>   
 
-        <p>Можно перейти к выбору теста.</p>
-        <hr>
-        <div>
-          <input type="submit" formaction="list.php" name="ShowTestsList" value="К тестам =>" title="Перейти к выполнению тестов">
-        </div>
-      </fieldset>
-
-</form>
-</div>
-
-</body>
+                  <?php 
+                if (!empty($_POST)) {
+                echo  
+                "<p> Количество правильных ответов: " . $trueAnswer . "</p>" .
+                "<p> Количество НЕ правильных ответов: " . $falseAnswer . "</p>" .
+                "<p> Нет ответов на " .$noAnswer. " вопросов </p>";
+                }  
+                ?>
+                                       
+            </div>
+        </form>
+    </body>
+</html>
